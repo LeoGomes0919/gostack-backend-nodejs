0da15f7b472bfd495eca511e22116518ff1d6ff9
LeoGomes0919 <leonardogs0919@gmail.com>
2021-02-08 23:58:01 -0300

    Cobertura do teste de criação de appointments.

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -14,5 +15,26 @@ describe('CreateAppointments', () => {
     });
 
     expect(appointment).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new appointment with the same date', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentDate = new Date(2021, 3, 8);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123456789',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123456789',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
